e41d4d520d329e5c1bc98d70bbc3a863391fdd90
hjtennent <harry.j.tennent@gmail.com>
2017-04-11 15:30:50 +0100

    Ep5 - Done - Uploading Images
    Added functionality to upload images and store them with the user’s
    other data in the Firebase database.

---
+++ gameofchats/LoginController+handlers.swift
@@ -0,0 +1,105 @@
+//
+//  LoginController+handlers.swift
+//  gameofchats
+//
+//  Created by Harry Tennent on 11/04/2017.
+//  Copyright © 2017 Harry Tennent. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+extension LoginController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func handleRegister() {
+        
+        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
+            print("Form is not valid.")
+            return
+        }
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user: FIRUser?, error) in
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            guard let uid = user?.uid else {
+                return
+            }
+            
+            //successfully authenticated user
+            let imageName = NSUUID().uuidString
+            let storageRef = FIRStorage.storage().reference().child("profile_images").child("\(imageName).png")
+            
+            if let uploadData = UIImagePNGRepresentation(self.profileImageView.image!) {
+                
+                storageRef.put(uploadData, metadata: nil, completion: { (metadata, error) in
+                    
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    
+                    if let profileImageURL = metadata?.downloadURL()?.absoluteString {
+                        let values = ["name":name, "email":email, "profileImageUrl":profileImageURL]
+                        
+                        self.registerUserIntoDatabaseWithUID(uid: uid, values: values)
+                        
+                    }
+                })
+            }
+        })
+    }
+    
+    
+    private func registerUserIntoDatabaseWithUID(uid: String, values:[String:Any]) {
+        let ref = FIRDatabase.database().reference(fromURL: "https://gameofchats-bd626.firebaseio.com/")
+        let usersReference = ref.child("users").child(uid)
+        
+        usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+            
+            if err != nil {
+                print(err!)
+                return
+            }
+            
+            
+            self.dismiss(animated: true, completion: nil)
+        })
+    }
+    
+    func handleSelectProfileImageView() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as! UIImage?	 {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as! UIImage? {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            profileImageView.image = selectedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("cancelled picker")
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- gameofchats/LoginController.swift
+++ gameofchats/LoginController.swift
@@ -58,42 +58,6 @@ class LoginController: UIViewController {
         
     }
     
-    func handleRegister() {
-        
-        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
-            print("Form is not valid.")
-            return
-        }
-        
-        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user: FIRUser?, error) in
-            
-            if error != nil {
-                print(error!)
-                return
-            }
-            
-            guard let uid = user?.uid else {
-                return
-            }
-            
-            //successfully authenticated user
-            let ref = FIRDatabase.database().reference(fromURL: "https://gameofchats-bd626.firebaseio.com/")
-            let usersReference = ref.child("users").child(uid)
-            let values = ["name":name, "email":email]
-            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
-                
-                if err != nil {
-                    print(err!)
-                    return
-                }
-                
-                
-                self.dismiss(animated: true, completion: nil)
-            })
-            
-        })
-    }
-    
     let nameTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Name"
@@ -131,13 +95,17 @@ class LoginController: UIViewController {
         return tf
     }()
     
-    let profileImageView: UIImageView = {
+    lazy var profileImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "gameofthrones_splash")
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
+        
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action:#selector(handleSelectProfileImageView)))
+        imageView.isUserInteractionEnabled = true
         return imageView
     }()
+    
     
     lazy var loginRegisterSegmentedControl: UISegmentedControl = {
         let sc = UISegmentedControl(items: ["Login", "Register"])
